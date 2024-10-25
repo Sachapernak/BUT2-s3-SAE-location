27095acf6aa0da5e7b963487ade1cfe7fc7f80b7
Lapatate82 <eliott.bourdon.novellas@gmail.com>
2024-10-25 14:59:04 +0200

    Ajout noms de contraintes

--- SQL/Generation tables.sql
+++ SQL/Generation tables.sql
@@ -12,7 +12,7 @@ CREATE TABLE SAE_Locataire(
    adresse_optionnelle VARCHAR2(50) ,
    Code_postal_optionnel NUMBER(10),
    ville_optionnel VARCHAR2(50) ,
-   PRIMARY KEY(identifiant_locataire)
+   CONSTRAINT pk_SAE_Locataire PRIMARY KEY(identifiant_locataire)
 );
 
 CREATE TABLE SAE_Bien_locatif(
@@ -24,7 +24,7 @@ CREATE TABLE SAE_Bien_locatif(
    surface NUMBER(10),
    nb_piece NUMBER(10),
    Type_de_bien VARCHAR2(50) ,
-   PRIMARY KEY(identifiant_logement)
+   CONSTRAINT pk_SAE_Bien_locatif PRIMARY KEY(identifiant_logement)
 );
 
 CREATE TABLE SAE_batiment(
@@ -33,7 +33,7 @@ CREATE TABLE SAE_batiment(
    ville VARCHAR2(50) ,
    code_postal VARCHAR2(50) ,
    numero_fiscal NUMBER(10),
-   PRIMARY KEY(identifiant_batiment)
+   CONSTRAINT pk_SAE_batiment PRIMARY KEY(identifiant_batiment)
 );
 
 CREATE TABLE SAE_Cautionnaire(
@@ -41,7 +41,7 @@ CREATE TABLE SAE_Cautionnaire(
    Nom_ou_organisme VARCHAR2(70)  NOT NULL,
    Prenom VARCHAR2(50) ,
    Description_du_cautionnaire CLOB,
-   PRIMARY KEY(Id_Caution)
+   CONSTRAINT pk_SAE_Cautionnaire PRIMARY KEY(Id_Caution)
 );
 
 CREATE TABLE SAE_Bail(
@@ -49,7 +49,7 @@ CREATE TABLE SAE_Bail(
    provision_pour_charge NUMBER(15,2)  ,
    Date_de_debut DATE,
    date_de_fin DATE,
-   PRIMARY KEY(Id_bail)
+   CONSTRAINT pk_SAE_Bail PRIMARY KEY(Id_bail)
 );
 
 CREATE TABLE SAE_assurance(
@@ -59,7 +59,7 @@ CREATE TABLE SAE_assurance(
    prime NUMBER(15,2)  ,
    quotite_jursiprudence NUMBER(3,2)  ,
    montant_quoitite NUMBER(15,2)  ,
-   PRIMARY KEY(numero_de_contrat, annee_du_contrat)
+   CONSTRAINT pk_SAE_assurance PRIMARY KEY(numero_de_contrat, annee_du_contrat)
 );
 
 CREATE TABLE SAE_entreprise(
@@ -69,7 +69,7 @@ CREATE TABLE SAE_entreprise(
    ville VARCHAR2(50) ,
    code_postal NUMBER(10),
    secteur_d_activite VARCHAR2(50) ,
-   PRIMARY KEY(SIRET)
+   CONSTRAINT pk_SAE_entreprise PRIMARY KEY(SIRET)
 );
 
 CREATE TABLE SAE_Document(
@@ -77,7 +77,7 @@ CREATE TABLE SAE_Document(
    Date_document DATE,
    type_de_document VARCHAR2(50) ,
    url_document BLOB,
-   PRIMARY KEY(Id_bail, Date_document),
+   CONSTRAINT pk_SAE_Document PRIMARY KEY(Id_bail, Date_document),
    CONSTRAINT fk_Sae_Document_Id_Bail FOREIGN KEY(Id_bail) REFERENCES SAE_Bail(Id_bail)
 );
 
@@ -85,7 +85,7 @@ CREATE TABLE SAE_Regularisation(
    Id_bail VARCHAR2(50) ,
    Date_regu DATE,
    montant VARCHAR2(50) ,
-   PRIMARY KEY(Id_bail, Date_regu),
+   CONSTRAINT pk_SAE_Regularisation PRIMARY KEY(Id_bail, Date_regu),
    CONSTRAINT fk_Sae_Regularisation_Id_Bail FOREIGN KEY(Id_bail) REFERENCES SAE_Bail(Id_bail)
 );
 
@@ -96,7 +96,7 @@ CREATE TABLE SAE_diagnostiques(
    fichier_diagnostique CLOB,
    identifiant_batiment VARCHAR2(50) ,
    identifiant_logement VARCHAR2(50) ,
-   PRIMARY KEY(Date_diagnostique, identifiant),
+   CONSTRAINT pk_SAE_diagnostiques PRIMARY KEY(Date_diagnostique, identifiant),
    CONSTRAINT fk_SAE_diag_id_batiment FOREIGN KEY(identifiant_batiment) REFERENCES SAE_batiment(identifiant_batiment),
    CONSTRAINT fk_SAE_diag_id_logement FOREIGN KEY(identifiant_logement) REFERENCES SAE_Bien_locatif(identifiant_logement)
 );
@@ -106,7 +106,7 @@ CREATE TABLE SAE_ICC(
    Annee_ICC CHAR(4) ,
    trimestre_ICC CHAR(1) ,
    indice NUMBER(10),
-   PRIMARY KEY(identifiant_logement, Annee_ICC, trimestre_ICC),
+   CONSTRAINT pk_SAE_ICC PRIMARY KEY(identifiant_logement, Annee_ICC, trimestre_ICC),
    CONSTRAINT fk_SAE_ICC_id_logement FOREIGN KEY(identifiant_logement) REFERENCES SAE_Bien_locatif(identifiant_logement)
 );
 
@@ -114,7 +114,7 @@ CREATE TABLE sae_loyer(
    identifiant_logement VARCHAR2(50) ,
    date_de_changement DATE,
    montant_loyer NUMBER(15,2)  ,
-   PRIMARY KEY(identifiant_logement, date_de_changement),
+   CONSTRAINT pk_sae_loyer PRIMARY KEY(identifiant_logement, date_de_changement),
    CONSTRAINT fk_sae_loyer_id_logement FOREIGN KEY(identifiant_logement) REFERENCES SAE_Bien_locatif(identifiant_logement)
 );
 
@@ -132,7 +132,7 @@ CREATE TABLE SAE_document_comptable(
    numero_de_contrat VARCHAR2(50) ,
    annee_du_contrat NUMBER(10),
    identifiant_logement VARCHAR2(50) ,
-   PRIMARY KEY(numero_document, Date_document),
+   CONSTRAINT pk_SAE_document_comptable PRIMARY KEY(numero_document, Date_document),
    CONSTRAINT fk_SAE_doc_compta_id_locataire FOREIGN KEY(identifiant_locataire) REFERENCES SAE_Locataire(identifiant_locataire),
    CONSTRAINT fk_SAE_doc_compta_id_batiment FOREIGN KEY(identifiant_batiment) REFERENCES SAE_batiment(identifiant_batiment),
    CONSTRAINT fk_SAE_doc_compta_SIRET FOREIGN KEY(SIRET) REFERENCES SAE_entreprise(SIRET),
@@ -156,8 +156,8 @@ CREATE TABLE sae_charge_index(
    Cout_fixe VARCHAR2(50) ,
    numero_document VARCHAR2(50)  NOT NULL,
    Date_document DATE NOT NULL,
-   PRIMARY KEY(Date_de_releve, Type),
-   UNIQUE(numero_document, Date_document),
+   CONSTRAINT pk_sae_charge_index PRIMARY KEY(Date_de_releve, Type),
+   CONSTRAINT un_SAE_chg_index_doc UNIQUE(numero_document, Date_document),
    CONSTRAINT fk_SAE_chg_index_doc_compta FOREIGN KEY(numero_document, Date_document) REFERENCES SAE_document_comptable(numero_document, Date_document)
 );
 
@@ -167,8 +167,8 @@ CREATE TABLE SAE_Charge_cf(
    montant VARCHAR2(50) ,
    numero_document VARCHAR2(50)  NOT NULL,
    Date_document DATE NOT NULL,
-   PRIMARY KEY(Date_de_charge, Type),
-   UNIQUE(numero_document, Date_document),
+   CONSTRAINT pk_SAE_Charge_cf PRIMARY KEY(Date_de_charge, Type),
+   CONSTRAINT un_SAE_chg_index_doc UNIQUE(numero_document, Date_document),
    CONSTRAINT fk_SAE_chg_cf_doc_compta FOREIGN KEY(numero_document, Date_document) REFERENCES SAE_document_comptable(numero_document, Date_document)
 );
 
@@ -177,7 +177,7 @@ CREATE TABLE sae_etre_lie(
    identifiant_locataire_1 VARCHAR2(50) ,
    Lien_familiale VARCHAR2(50) ,
    Colocataire NUMBER(1) NOT NULL,
-   PRIMARY KEY(identifiant_locataire, identifiant_locataire_1),
+   CONSTRAINT pk_sae_etre_lie PRIMARY KEY(identifiant_locataire, identifiant_locataire_1),
    CONSTRAINT fk_sae_etre_lie_id_locataire FOREIGN KEY(identifiant_locataire) REFERENCES SAE_Locataire(identifiant_locataire),
    CONSTRAINT fk_sae_etre_lie_id_locataire2 FOREIGN KEY(identifiant_locataire_1) REFERENCES SAE_Locataire(identifiant_locataire)
 );
@@ -187,7 +187,7 @@ CREATE TABLE sae_Cautionner(
    Id_bail VARCHAR2(50) ,
    Montant NUMBER(15,2)  ,
    Fichier_caution CLOB,
-   PRIMARY KEY(Id_Caution, Id_bail),
+   CONSTRAINT pk_sae_Cautionner PRIMARY KEY(Id_Caution, Id_bail),
    CONSTRAINT fk_sae_Cautionner_Id_Caution FOREIGN KEY(Id_Caution) REFERENCES SAE_Cautionnaire(Id_Caution),
    CONSTRAINT fk_sae_Cautionner_Id_bail FOREIGN KEY(Id_bail) REFERENCES SAE_Bail(Id_bail)
 );
@@ -196,7 +196,7 @@ CREATE TABLE sae_appartenir(
    identifiant_logement VARCHAR2(50) ,
    identifiant_batiment VARCHAR2(50) ,
    part_des_charges NUMBER(3,2)  ,
-   PRIMARY KEY(identifiant_logement, identifiant_batiment),
+   CONSTRAINT pk_sae_appartenir PRIMARY KEY(identifiant_logement, identifiant_batiment),
    CONSTRAINT fk_sae_appartenir_id_logement FOREIGN KEY(identifiant_logement) REFERENCES SAE_Bien_locatif(identifiant_logement),
    CONSTRAINT fk_sae_appartenir_id_batiment FOREIGN KEY(identifiant_batiment) REFERENCES SAE_batiment(identifiant_batiment)
 );
@@ -207,7 +207,7 @@ CREATE TABLE sae_contracter(
    date_de_sortie DATE,
    date_d_entree DATE,
    part_de_loyer NUMBER(3,2)  ,
-   PRIMARY KEY(identifiant_locataire, Id_bail),
+   CONSTRAINT pk_sae_contracter PRIMARY KEY(identifiant_locataire, Id_bail),
    CONSTRAINT fk_sae_contracter_id_locataire FOREIGN KEY(identifiant_locataire) REFERENCES SAE_Locataire(identifiant_locataire),
    CONSTRAINT fk_sae_contracter_Id_bail FOREIGN KEY(Id_bail) REFERENCES SAE_Bail(Id_bail)
 );
@@ -215,7 +215,7 @@ CREATE TABLE sae_contracter(
 CREATE TABLE sae_mettre_en_location(
    identifiant_logement VARCHAR2(50) ,
    Id_bail VARCHAR2(50) ,
-   PRIMARY KEY(identifiant_logement, Id_bail),
+   CONSTRAINT pk_sae_mettre_en_location PRIMARY KEY(identifiant_logement, Id_bail),
    CONSTRAINT fk_sae_mettre_loc_id_logement FOREIGN KEY(identifiant_logement) REFERENCES SAE_Bien_locatif(identifiant_logement),
    CONSTRAINT fk_sae_mettre_loc_Id_bail FOREIGN KEY(Id_bail) REFERENCES SAE_Bail(Id_bail)
 );
@@ -225,7 +225,7 @@ CREATE TABLE sae_comprendre_charge_variable(
    Date_de_releve DATE,
    Type VARCHAR2(50) ,
    part_des_charges NUMBER(3,2)  ,
-   PRIMARY KEY(identifiant_logement, Date_de_releve, Type),
+   CONSTRAINT pk_sae_comprendre_charge_variable PRIMARY KEY(identifiant_logement, Date_de_releve, Type),
    CONSTRAINT fk_sae_compr_ch_vr_id_logement FOREIGN KEY(identifiant_logement) REFERENCES SAE_Bien_locatif(identifiant_logement),
    CONSTRAINT fk_sae_compr_ch_vr_date FOREIGN KEY(Date_de_releve, Type) REFERENCES sae_charge_index(Date_de_releve, Type)
 );
@@ -235,7 +235,7 @@ CREATE TABLE sae_comprendre_cf(
    Date_de_charge DATE,
    Type VARCHAR2(50) ,
    part_des_charges NUMBER(3,2)  ,
-   PRIMARY KEY(identifiant_logement, Date_de_charge, Type),
+   CONSTRAINT pk_sae_comprendre_cf PRIMARY KEY(identifiant_logement, Date_de_charge, Type),
    CONSTRAINT fk_sae_compr_cf_id_logement FOREIGN KEY(identifiant_logement) REFERENCES SAE_Bien_locatif(identifiant_logement),
    CONSTRAINT fk_sae_compr_cf_Date FOREIGN KEY(Date_de_charge, Type) REFERENCES SAE_Charge_cf(Date_de_charge, Type)
 );
