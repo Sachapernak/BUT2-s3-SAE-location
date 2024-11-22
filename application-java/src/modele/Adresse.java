cd50102b5b8268d55a795a57ea2b0ce18f12d1e3
Sacha Pernak <sacha.pernak@etu.iut-tlse3.fr>
2024-11-22 15:25:26 +0100

    Ajout de la classe locataire et Adresse

--- application-java/src/modele/Adresse.java
+++ application-java/src/modele/Adresse.java
@@ -1,5 +1,123 @@
 package modele;
 
+import java.util.Objects;
+
 public class Adresse {
+	
+	private String Id_adresse;
+	private String adresse;
+	private int Code_postal;
+	private String ville;
+	private String Complement_adresse;
+	
+	public Adresse(String Id_adresse, String adresse, int Code_postal, String ville, String Complement_adresse) {
+		this.Id_adresse = Id_adresse;
+		this.adresse =adresse;
+		this.Code_postal = Code_postal;
+		this.ville = ville;
+		this.Complement_adresse = Complement_adresse;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Adresse [Id_adresse=" + Id_adresse + ", adresse=" + adresse + ", Code_postal=" + Code_postal
+				+ ", ville=" + ville + ", Complement_adresse=" + Complement_adresse + "]";
+	}
+
+
+	/**
+	 * @return the id_adresse
+	 */
+	public String getId_adresse() {
+		return Id_adresse;
+	}
+
+
+	/**
+	 * @return the adresse
+	 */
+	public String getAdresse() {
+		return adresse;
+	}
+
+
+	/**
+	 * @return the code_postal
+	 */
+	public int getCode_postal() {
+		return Code_postal;
+	}
+
+
+	/**
+	 * @return the ville
+	 */
+	public String getVille() {
+		return ville;
+	}
+
+
+	/**
+	 * @return the complement_adresse
+	 */
+	public String getComplement_adresse() {
+		return Complement_adresse;
+	}
+
+
+	/**
+	 * @param adresse the adresse to set
+	 */
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+
+	/**
+	 * @param code_postal the code_postal to set
+	 */
+	public void setCode_postal(int code_postal) {
+		Code_postal = code_postal;
+	}
+
+
+	/**
+	 * @param ville the ville to set
+	 */
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+
+	/**
+	 * @param complement_adresse the complement_adresse to set
+	 */
+	public void setComplement_adresse(String complement_adresse) {
+		Complement_adresse = complement_adresse;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(Code_postal, Complement_adresse, Id_adresse, adresse, ville);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Adresse)) {
+			return false;
+		}
+		Adresse other = (Adresse) obj;
+		return this.Id_adresse == other.Id_adresse;
+	}
+	
+	
+	
+	
 
 }
--- application-java/src/modele/Locataire.java
+++ application-java/src/modele/Locataire.java
@@ -1,125 +1,209 @@
 package modele;
 
-import java.util.List;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Objects;
 
 public class Locataire {
-	private String id;
+	private String Id_locataire;
 	private String nom;
 	private String prenom;
 	private String email;
 	private String telephone;
-	private String dateNaissance; // est une date ?
-	private String lieuNaissance;
-	private String fichierCaution;
+	private String date_naissance;
+	private String lieu_de_naissance;
+	private String acte_de_caution;
+	private String Id_adresse;
+	private Collection<Bail> Id_bail;
 	
-	private List<EtreLie> liens;
 	
-	private Adresse adresse;
-	
-	public Locataire(String id, String nom, String prenom, String dateNaissance) {
+	public Locataire(String Id_locataire, String nom, String prenom, String email, String telephone, String date_naissance, String lieu_de_naissance, String acte_de_caution, String Id_adresse) {
+		this.Id_locataire = Id_locataire;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.telephone = telephone;
+		this.date_naissance = date_naissance;
+		this.acte_de_caution = acte_de_caution;
+		this.Id_adresse = Id_adresse;
+		this.Id_bail = new HashSet<Bail>();
 		
 	}
 
-	
-	
+
+	/**
+	 * @return the id_locataire
+	 */
+	public String getId_locataire() {
+		return Id_locataire;
+	}
+
+
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+
+
+	/**
+	 * @return the prenom
+	 */
+	public String getPrenom() {
+		return prenom;
+	}
+
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+
+	/**
+	 * @return the telephone
+	 */
+	public String getTelephone() {
+		return telephone;
+	}
+
+
+	/**
+	 * @return the date_naissance
+	 */
+	public String getDate_naissance() {
+		return date_naissance;
+	}
+
+
+	/**
+	 * @return the lieu_de_naissance
+	 */
+	public String getLieu_de_naissance() {
+		return lieu_de_naissance;
+	}
+
+
+	/**
+	 * @return the acte_de_caution
+	 */
+	public String getActe_de_caution() {
+		return acte_de_caution;
+	}
+
+
+	/**
+	 * @return the id_adresse
+	 */
+	public String getId_adresse() {
+		return Id_adresse;
+	}
+
+
+	/**
+	 * @return the id_bail
+	 */
+	public Collection<Bail> getId_bail() {
+		return Id_bail;
+	}
+
+
+	/**
+	 * @param nom the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+	/**
+	 * @param prenom the prenom to set
+	 */
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	/**
+	 * @param telephone the telephone to set
+	 */
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+
+	/**
+	 * @param date_naissance the date_naissance to set
+	 */
+	public void setDate_naissance(String date_naissance) {
+		this.date_naissance = date_naissance;
+	}
+
+
+	/**
+	 * @param lieu_de_naissance the lieu_de_naissance to set
+	 */
+	public void setLieu_de_naissance(String lieu_de_naissance) {
+		this.lieu_de_naissance = lieu_de_naissance;
+	}
+
+
+	/**
+	 * @param acte_de_caution the acte_de_caution to set
+	 */
+	public void setActe_de_caution(String acte_de_caution) {
+		this.acte_de_caution = acte_de_caution;
+	}
+
+
+	/**
+	 * @param id_bail the id_bail to set
+	 */
+	public void setId_bail(Collection<Bail> id_bail) {
+		Id_bail = id_bail;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Locataire [Id_locataire=" + Id_locataire + ", nom=" + nom + ", prenom=" + prenom + ", email=" + email
+				+ ", telephone=" + telephone + ", date_naissance=" + date_naissance + ", lieu_de_naissance="
+				+ lieu_de_naissance + ", acte_de_caution=" + acte_de_caution + ", Id_adresse=" + Id_adresse
+				+ ", Id_bail=" + Id_bail + "]";
+	}
+
+
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		return Objects.hash(Id_adresse, Id_bail, Id_locataire, acte_de_caution, date_naissance, email,
+				lieu_de_naissance, nom, prenom, telephone);
 	}
 
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
+		if (this == obj) {
 			return true;
-		if (obj == null)
+		}
+		if (!(obj instanceof Locataire)) {
 			return false;
-		if (getClass() != obj.getClass())
-			return false;
+		}
 		Locataire other = (Locataire) obj;
-		return Objects.equals(id, other.id);
-	}
-
-
-	// Getter / Setters
-	public String getNom() {
-		return nom;
-	}
-
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-
-	public String getPrenom() {
-		return prenom;
-	}
-
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getTelephone() {
-		return telephone;
-	}
-
-	public void setTelephone(String telephone) {
-		this.telephone = telephone;
-	}
-
-	public String getDateNaissance() {
-		return dateNaissance;
-	}
-
-	public void setDateNaissance(String dateNaissance) {
-		this.dateNaissance = dateNaissance;
-	}
-
-	public String getLieuNaissance() {
-		return lieuNaissance;
-	}
-
-	public void setLieuNaissance(String lieuNaissance) {
-		this.lieuNaissance = lieuNaissance;
-	}
-
-	public String getFichierCaution() {
-		return fichierCaution;
-	}
-
-	public void setFichierCaution(String fichierCaution) {
-		this.fichierCaution = fichierCaution;
-	}
-
-	public List<EtreLie> getLiens() {
-		return liens;
-	}
-
-	public void setLiens(List<EtreLie> liens) {
-		this.liens = liens;
-	}
-
-	public Adresse getAdresse() {
-		return adresse;
-	}
-
-	public void setAdresse(Adresse adresse) {
-		this.adresse = adresse;
-	}
-
-	public String getId() {
-		return id;
+		return this.Id_locataire == other.Id_locataire;
 	}
 	
 	
 	
+
 }
-
