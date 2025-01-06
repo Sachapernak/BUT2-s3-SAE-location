eab206b1e3692663c605b726b7b84d38326471b8
Sacha Pernak <sacha.pernak@etu.iut-tlse3.fr>
2025-01-06 10:48:56 +0100

    Model MVC Complet

--- application-java/src/controleur/GestionPageConnexion.java
+++ application-java/src/controleur/GestionPageConnexion.java
@@ -9,7 +9,6 @@ import java.util.Arrays;
 import java.util.Optional;
 
 import javax.swing.JButton;
-import javax.swing.JPasswordField;
 import javax.swing.SwingWorker;
 
 import modele.ConnexionBD;
@@ -17,211 +16,200 @@ import modele.FichierConfig;
 import vue.PageConnexion;
 
 public class GestionPageConnexion {
-	
-	
-	private PageConnexion pageConnexion;
-	
+    
+    private PageConnexion pageConnexion;
+    
+    public GestionPageConnexion(PageConnexion pageConnexion) {
+        this.pageConnexion = pageConnexion;
+    }
 
-	public GestionPageConnexion(PageConnexion pageConnexion) {
-		
-		this.pageConnexion = pageConnexion;
-		
-		
-	}
+    // -------------------------------------------------------------------------
+    // Gestions d'événements (souris, clics...)
+    // -------------------------------------------------------------------------
 
-	public void gestionSourisQuitter(JButton btnQuitter) {
-			btnQuitter.addMouseListener(new MouseAdapter() {
-	        	@Override
-	        	public void mouseClicked(MouseEvent e) {
-	        		pageConnexion.dispose();
-	        	}
-	        });
-		
-	
-	}
-
-	public void chargerInfoConnexion() {
-		FichierConfig fc = FichierConfig.getInstance();
-        
-        try {
-			String link = fc.lire("DB_LINK");
-			String user = fc.lire("DB_USER");
-			
-			pageConnexion.getTextFieldLien().setText(link);
-			pageConnexion.getTextFieldID().setText(user);
-			
-			
-		} catch (IOException e) {			
-			pageConnexion.afficherMessageErreur(e.getMessage());
-			
-		}
-	}
-
-	public void gestionSourisRecharger(JButton btnRefresh) {
-		btnRefresh.addMouseListener(new MouseAdapter() {
-        	@Override
-        	public void mouseClicked(MouseEvent e) {
-        		
-                recharger();
-        	}
-
-
+    public void gestionSourisQuitter(JButton btnQuitter) {
+        btnQuitter.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                pageConnexion.dispose();
+            }
         });
-		
-	}
-	
-	private void recharger() {
-		pageConnexion.getLblSatutText().setText("Chargement...");
-		pageConnexion.getLblSatutText().setForeground(new Color(255, 128, 0));
-        
-		pageConnexion.getLblLatCoVal().setText("Chargement...");
-		pageConnexion.getLblLatCoVal().setForeground(new Color(255, 128, 0));
-        
-		pageConnexion.getLblLatReqVal().setText("Chargement...");
-		pageConnexion.getLblLatReqVal().setForeground(new Color(255, 128, 0));
-        
+    }
+
+    public void gestionSourisRecharger(JButton btnRefresh) {
+        btnRefresh.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                recharger();
+            }
+        });
+    }
+
+    public void gestionSourisConfirmer(JButton btnConfirmer) {
+        btnConfirmer.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                // On récupère les valeurs directement via la Vue
+                String lien = pageConnexion.getLien();
+                String user = pageConnexion.getIdentifiant();
+                char[] pwd = pageConnexion.getMotDePasse();
+                
+                try {
+                    FichierConfig fc = FichierConfig.getInstance();
+                    
+                    if (!lien.isEmpty()) {
+                        fc.enregistrer("DB_LINK", lien);
+                    }
+                    
+                    if (!user.isEmpty()) {
+                        fc.enregistrer("DB_USER", user);
+                    }
+                    
+                    if (pwd.length > 0) {
+                        try {
+                            fc.enregistrerMdp(new String(pwd));
+                        } finally {
+                            Arrays.fill(pwd, '\0');  // Nettoyer le tableau
+                        }
+                    }
+                    
+                    ConnexionBD bd = ConnexionBD.getInstance();
+                    bd.updateBDLink();
+                    
+                    recharger();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+    }
+
+    // -------------------------------------------------------------------------
+    // Chargement / Rechargement des infos
+    // -------------------------------------------------------------------------
+
+    public void chargerInfoConnexion() {
+        FichierConfig fc = FichierConfig.getInstance();
+        try {
+            String link = fc.lire("DB_LINK");
+            String user = fc.lire("DB_USER");
+            
+            // On met à jour la Vue pour afficher le lien et l'ID
+            pageConnexion.setLien(link);
+            pageConnexion.setIdentifiant(user);
+
+        } catch (IOException e) {
+            pageConnexion.afficherMessageErreur(e.getMessage());
+        }
+    }
+
+    private void recharger() {
+        // Mise à jour du statut et des latences
+        pageConnexion.setStatut("Chargement...", new Color(255, 128, 0));
+        pageConnexion.setLatenceConnexion("Chargement...", new Color(255, 128, 0));
+        pageConnexion.setLatenceRequete("Chargement...", new Color(255, 128, 0));
+
         getInformationBD();
         chargerInfoConnexion();
-	}
-	
-	public void getInformationBD() {
-		new SwingWorker<Void, Void>() {
-            @Override
-            protected Void doInBackground() throws Exception {
-                setConnexion();
-                setValLatCo();
-                setValLatReq();
-                return null;
+    }
+    
+    private boolean setConnexion() {
+        try {
+            if (ConnexionBD.getInstance().isConnexionOk()) {
+                pageConnexion.setStatut("Connexion OK", new Color(0, 128, 0));
+                return true;
+                
+            } else {
+                pageConnexion.setStatut("Pas de connexion", Color.RED);
+                pageConnexion.setLatenceConnexion("N/A", Color.RED);
+                pageConnexion.setLatenceRequete("N/A", Color.RED);
+                return false;
             }
-        }.execute();
-	}
-	
+        } catch (SQLException | IOException e) {
+            pageConnexion.setStatut("Pas de connexion", Color.RED);
+            pageConnexion.setLatenceConnexion("N/A", Color.RED);
+            pageConnexion.setLatenceRequete("N/A", Color.RED);
+            return false;
+        }
+    }
+    
     private void setValLatCo() {
-        
         final long GOOD_PING = 500;
         final long BAD_PING = 1000;
         
         try {
             Optional<Long> val = ConnexionBD.getInstance().latenceConnexionBD();
-            
             if (val.isPresent()) {
-            	pageConnexion.getLblLatCoVal().setText(val.get().toString());
-            	pageConnexion.getLblms1().setVisible(true);
-                
-                if (val.get() < GOOD_PING) {
-                	pageConnexion.getLblLatCoVal().setForeground(new Color(0, 128, 0));
-                } else if (val.get() < BAD_PING){
-                	pageConnexion.getLblLatCoVal().setForeground(new Color(255, 128, 0));
+                long ping = val.get();
+                // Couleur en fonction du ping
+                Color c;
+                if (ping < GOOD_PING) {
+                    c = new Color(0, 128, 0);
+                } else if (ping < BAD_PING) {
+                    c = new Color(255, 128, 0);
                 } else {
-                	pageConnexion.getLblLatCoVal().setForeground(Color.RED);
+                    c = Color.RED;
                 }
+                pageConnexion.setLatenceConnexion(String.valueOf(ping), c);
                 
             } else {
-            	pageConnexion.getLblSatutText().setText("Pas de connexion");
-            	pageConnexion.getLblSatutText().setForeground(Color.RED);
-            	
-            	pageConnexion.getLblLatCoVal().setText("N/A");
-            	pageConnexion.getLblLatCoVal().setForeground(Color.RED);
+                pageConnexion.setStatut("Pas de connexion", Color.RED);
+                pageConnexion.setLatenceConnexion("N/A", Color.RED);
+
             }
         } catch (SQLException | IOException e) {
-        	pageConnexion.getLblSatutText().setText("Pas de connexion");
-        	pageConnexion.getLblSatutText().setForeground(Color.RED);
-            
-        	pageConnexion.getLblLatCoVal().setText("N/A");
-        	pageConnexion.getLblLatCoVal().setForeground(Color.RED);
-            
+            pageConnexion.setStatut("Pas de connexion", Color.RED);
+            pageConnexion.setLatenceConnexion("N/A", Color.RED);
+
         }
     }
     
     private void setValLatReq() {
-        
         final long GOOD_PING = 50;
         final long BAD_PING = 100;
         
         try {
             Optional<Long> val = ConnexionBD.getInstance().latenceRequeteBD();
-            
             if (val.isPresent()) {
-            	pageConnexion.getLblLatReqVal().setText(val.get().toString());
-            	pageConnexion.getLblms2().setVisible(true);
-                
-                if (val.get() < GOOD_PING) {
-                	pageConnexion.getLblLatReqVal().setForeground(new Color(0, 128, 0));
-                } else if (val.get() < BAD_PING){
-                	pageConnexion.getLblLatReqVal().setForeground(new Color(255, 128, 0));
+                long ping = val.get();
+                Color c;
+                if (ping < GOOD_PING) {
+                    c = new Color(0, 128, 0);
+                } else if (ping < BAD_PING) {
+                    c = new Color(255, 128, 0);
                 } else {
-                	pageConnexion.getLblLatReqVal().setForeground(Color.RED);
+                    c = Color.RED;
                 }
-            } else {
-            	pageConnexion.getLblSatutText().setText("Pas de connexion");
-            	pageConnexion.getLblSatutText().setForeground(Color.RED);
+                pageConnexion.setLatenceRequete(String.valueOf(ping), c);
                 
-            	pageConnexion.getLblLatReqVal().setText("N/A");
-            	pageConnexion.getLblLatReqVal().setForeground(Color.RED);
-            }
-        } catch (SQLException | IOException e) {
-        	pageConnexion.getLblSatutText().setText("Pas de connexion");
-        	pageConnexion.getLblSatutText().setForeground(Color.RED);
-            
-        	pageConnexion.getLblLatReqVal().setText("N/A");
-        	pageConnexion.getLblLatReqVal().setForeground(Color.RED);
-        }
-    }
-
-	public void gestionSourisConfirmer(JButton btnConfirmer, JPasswordField passwordField) {
-		btnConfirmer.addMouseListener(new MouseAdapter() {
-        	@Override
-        	public void mouseClicked(MouseEvent e) {
-        		String lien = pageConnexion.getTextFieldLien().getText();
-        		String user = pageConnexion.getTextFieldID().getText();
-        		
-
-        		
-        		try {
-        			FichierConfig fc = FichierConfig.getInstance();
-        			if (lien.length() > 0) {
-        				fc.enregistrer("DB_LINK", lien);
-        			}
-        			
-        			if (user.length() > 0) {
-        				fc.enregistrer("DB_USER", user);
-        			}
-        			
-        			if (passwordField.getPassword().length > 0) {
-        				try {
-        				fc.enregistrerMdp(new String(passwordField.getPassword()));
-        				} finally {
-        					Arrays.fill(passwordField.getPassword() , '\0');
-        				}
-        				
-        			}
-        			
-            		ConnexionBD bd = ConnexionBD.getInstance();
-            		bd.updateBDLink();
-            		
-            		recharger();
-            		
-        			
-        		} catch (Exception e1) {
-        			e1.printStackTrace();
-        		}
-        	}
-        });
-		
-	}
-	
-    private void setConnexion() {
-        try {
-            if (ConnexionBD.getInstance().isConnexionOk()) {
-            	pageConnexion.getLblSatutText().setText("Connexion OK");
-            	pageConnexion.getLblSatutText().setForeground(new Color(0, 128, 0));
             } else {
-            	pageConnexion.getLblSatutText().setText("Pas de connexion");
-            	pageConnexion.getLblSatutText().setForeground(Color.RED);
+                pageConnexion.setStatut("Pas de connexion", Color.RED);
+                pageConnexion.setLatenceRequete("N/A", Color.RED);
             }
         } catch (SQLException | IOException e) {
-        	pageConnexion.getLblSatutText().setText("Pas de connexion");
-        	pageConnexion.getLblSatutText().setForeground(Color.RED);
+            pageConnexion.setStatut("Pas de connexion", Color.RED);
+            pageConnexion.setLatenceRequete("N/A", Color.RED);
         }
     }
+
+    // -------------------------------------------------------------------------
+    // SwingWorker pour récupérer infos BD sans bloquer la vue
+    // -------------------------------------------------------------------------
+
+    public void getInformationBD() {
+        new SwingWorker<Void, Void>() {
+            @Override
+            protected Void doInBackground() throws Exception {
+                if (setConnexion()) {
+	                setValLatCo();
+	                setValLatReq();
+                }
+                return null;
+            }
+        }.execute();
+    }
+
+    
+    
+
 }
--- application-java/src/vue/PageConnexion.java
+++ application-java/src/vue/PageConnexion.java
@@ -1,42 +1,43 @@
 package vue;
 
 import java.awt.EventQueue;
-import javax.swing.JInternalFrame;
 import java.awt.BorderLayout;
-import javax.swing.JPanel;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Rectangle;
+
+import javax.swing.JInternalFrame;
+import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-
-import java.awt.FlowLayout;
-import java.awt.Color;
-import controleur.GestionPageConnexion;
-import java.awt.Rectangle;
 import javax.swing.JButton;
-import javax.swing.SwingConstants;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
-import java.awt.Font;
+import javax.swing.SwingConstants;
+
+import controleur.GestionPageConnexion;
 
 public class PageConnexion extends JInternalFrame {
 
     private static final long serialVersionUID = 1L;
 
-	private JLabel lblSatutText;
+    // Champs graphiques 
+    private JLabel lblSatutText;
     private JLabel lblms1;
     private JLabel lblms2;
     private JLabel lblLatReqVal;
     private JLabel lblLatCoVal;
+    
     private JTextField textFieldLien;
     private JTextField textFieldID;
     private JPasswordField passwordField;
-    
-    private GestionPageConnexion gest;
-    
 
-    /**
-     * Launch the application.
-     */
+    //  Contrôleur 
+    private GestionPageConnexion gest;
+
+
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -51,45 +52,47 @@ public class PageConnexion extends JInternalFrame {
     }
 
     /**
-     * Create the frame.
+     * Constructeur de la fenêtre (Vue).
      */
     public PageConnexion() {
-    	
-    	this.gest = new GestionPageConnexion(this);
-    	
-    	getContentPane().setBounds(new Rectangle(0, 0, 400, 400));
+        
+        // Instanciation du contrôleur
+        this.gest = new GestionPageConnexion(this);
 
+        getContentPane().setBounds(new Rectangle(0, 0, 400, 400));
         setBounds(100, 100, 400, 300);
         setResizable(false);
         getContentPane().setLayout(new BorderLayout(0, 0));
         
+        // ---- Panel du haut (vide) -------------------------------------------
         JPanel panelTop = new JPanel();
         getContentPane().add(panelTop, BorderLayout.NORTH);
         
+        // ---- Panel du bas (boutons) -----------------------------------------
         JPanel panelBot = new JPanel();
         getContentPane().add(panelBot, BorderLayout.SOUTH);
         panelBot.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
         
         JButton btnQuitter = new JButton("Quitter");
-
         panelBot.add(btnQuitter);
         
         JButton btnRefresh = new JButton("Recharger");
-
         panelBot.add(btnRefresh);
         
         JButton btnConfirmer = new JButton("Confirmer");
-
         panelBot.add(btnConfirmer);
         
+        // ---- Panel central (toute la grille) --------------------------------
         JPanel panelMid = new JPanel();
         getContentPane().add(panelMid, BorderLayout.CENTER);
         panelMid.setLayout(new GridLayout(3, 0, 0, 0));
         
+        // ----------------------- G1 : Statut + Latences -----------------------
         JPanel panelG1 = new JPanel();
         panelMid.add(panelG1);
         panelG1.setLayout(new GridLayout(3, 0, 0, 0));
         
+        // Statut
         JPanel panelG11 = new JPanel();
         panelG1.add(panelG11);
         panelG11.setLayout(new GridLayout(0, 2, 5, 0));
@@ -102,6 +105,7 @@ public class PageConnexion extends JInternalFrame {
         lblSatutText.setForeground(new Color(255, 128, 0));
         panelG11.add(lblSatutText);
         
+        // Latence connexion
         JPanel panelG12 = new JPanel();
         panelG1.add(panelG12);
         panelG12.setLayout(new GridLayout(0, 2, 5, 0));
@@ -110,19 +114,19 @@ public class PageConnexion extends JInternalFrame {
         lblLatCoTitre.setHorizontalAlignment(SwingConstants.TRAILING);
         panelG12.add(lblLatCoTitre);
         
-        JPanel panel = new JPanel();
-        panelG12.add(panel);
-        panel.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 6));
+        JPanel panelLatCo = new JPanel();
+        panelG12.add(panelLatCo);
+        panelLatCo.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 6));
         
         lblLatCoVal = new JLabel("Calcul...");
         lblLatCoVal.setForeground(new Color(255, 128, 0));
-        panel.add(lblLatCoVal);
+        panelLatCo.add(lblLatCoVal);
         
         lblms1 = new JLabel("ms");
         lblms1.setForeground(Color.BLACK);
-        panel.add(lblms1);
+        panelLatCo.add(lblms1);
         
-        
+        // Latence requête
         JPanel panelG13 = new JPanel();
         panelG1.add(panelG13);
         panelG13.setLayout(new GridLayout(0, 2, 5, 0));
@@ -131,24 +135,26 @@ public class PageConnexion extends JInternalFrame {
         lblLatReqTitre.setHorizontalAlignment(SwingConstants.TRAILING);
         panelG13.add(lblLatReqTitre);
         
-        JPanel panelG132 = new JPanel();
-        FlowLayout flowLayout = (FlowLayout) panelG132.getLayout();
+        JPanel panelLatReq = new JPanel();
+        FlowLayout flowLayout = (FlowLayout) panelLatReq.getLayout();
         flowLayout.setVgap(6);
         flowLayout.setAlignment(FlowLayout.LEFT);
-        panelG13.add(panelG132);
+        panelG13.add(panelLatReq);
         
         lblLatReqVal = new JLabel("Calcul...");
         lblLatReqVal.setForeground(new Color(255, 128, 0));
-        panelG132.add(lblLatReqVal);
+        panelLatReq.add(lblLatReqVal);
         
         lblms2 = new JLabel("ms");
         lblms2.setForeground(Color.BLACK);
-        panelG132.add(lblms2);
+        panelLatReq.add(lblms2);
         
+        // ----------------------- G2 : Titre + Lien ----------------------------
         JPanel panelG2 = new JPanel();
         panelMid.add(panelG2);
         panelG2.setLayout(new GridLayout(2, 0, 0, 0));
         
+        //  Titre
         JPanel panelG21 = new JPanel();
         panelG2.add(panelG21);
         panelG21.setLayout(new BorderLayout(0, 0));
@@ -158,6 +164,7 @@ public class PageConnexion extends JInternalFrame {
         lblBD.setHorizontalAlignment(SwingConstants.CENTER);
         panelG21.add(lblBD);
         
+        //  Saisie lien
         JPanel panelG22 = new JPanel();
         panelG2.add(panelG22);
         panelG22.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 5));
@@ -166,13 +173,15 @@ public class PageConnexion extends JInternalFrame {
         panelG22.add(lblLien);
         
         textFieldLien = new JTextField();
-        panelG22.add(textFieldLien);
         textFieldLien.setColumns(30);
+        panelG22.add(textFieldLien);
         
+        // ----------------------- G3 : Saisie ID + MDP -------------------------
         JPanel panelG3 = new JPanel();
         panelMid.add(panelG3);
         panelG3.setLayout(new GridLayout(2, 0, 0, 0));
         
+        // Saisie identifiant
         JPanel panelG31 = new JPanel();
         panelG3.add(panelG31);
         panelG31.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 5));
@@ -181,9 +190,10 @@ public class PageConnexion extends JInternalFrame {
         panelG31.add(lblID);
         
         textFieldID = new JTextField();
-        panelG31.add(textFieldID);
         textFieldID.setColumns(30);
+        panelG31.add(textFieldID);
         
+        // Saisie mot de passe
         JPanel panelG32 = new JPanel();
         panelG3.add(panelG32);
         panelG32.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 5));
@@ -195,53 +205,66 @@ public class PageConnexion extends JInternalFrame {
         passwordField.setColumns(30);
         panelG32.add(passwordField);
         
+        // On cache les "ms" par défaut
         lblms1.setVisible(false);
         lblms2.setVisible(false);
         
-        
+        // Actions du contrôleur 
         gest.gestionSourisQuitter(btnQuitter);
-        
         gest.chargerInfoConnexion();
-        
-        
         gest.gestionSourisRecharger(btnRefresh);
-        gest.gestionSourisConfirmer(btnConfirmer, passwordField);
+        gest.gestionSourisConfirmer(btnConfirmer);
 
-        // Utiliser SwingWorker pour exécuter les tâches en arrière-plan
         gest.getInformationBD();
     }
 
+
+    
+    // Méthodes pour récupérer les valeurs saisies 
+    public String getLien() {
+        return textFieldLien.getText();
+    }
+
+    public String getIdentifiant() {
+        return textFieldID.getText();
+    }
+
+    public char[] getMotDePasse() {
+        return passwordField.getPassword();
+    }
+
+    // Méthodes pour mettre à jour l’affichage (labels) 
+    public void setStatut(String statut, Color couleur) {
+        lblSatutText.setText(statut);
+        lblSatutText.setForeground(couleur);
+    }
+
+    public void setLatenceConnexion(String latence, Color couleur) {
+        lblLatCoVal.setText(latence);
+        lblLatCoVal.setForeground(couleur);
+        lblms1.setVisible(true);
+    }
+
+    public void setLatenceRequete(String latence, Color couleur) {
+        lblLatReqVal.setText(latence);
+        lblLatReqVal.setForeground(couleur);
+        lblms2.setVisible(true);
+    }
+    
+    public void setLien(String lien) {
+        textFieldLien.setText(lien);
+    }
+
+    public void setIdentifiant(String identifiant) {
+        textFieldID.setText(identifiant);
+    }
+
+
+    // Méthodes utilitaires 
     public void afficherMessageErreur(String message) {
-        JOptionPane.showMessageDialog(this, "Erreur de lecture : \n" + message, "Erreur", JOptionPane.ERROR_MESSAGE);
+        JOptionPane.showMessageDialog(this, "Erreur de lecture : \n" + message, 
+                                      "Erreur", JOptionPane.ERROR_MESSAGE);
     }
     
     
-    public JLabel getLblSatutText() {
-		return lblSatutText;
-	}
-
-	public JLabel getLblms1() {
-		return lblms1;
-	}
-
-	public JLabel getLblms2() {
-		return lblms2;
-	}
-
-	public JLabel getLblLatReqVal() {
-		return lblLatReqVal;
-	}
-
-	public JLabel getLblLatCoVal() {
-		return lblLatCoVal;
-	}
-
-	public JTextField getTextFieldLien() {
-		return textFieldLien;
-	}
-
-	public JTextField getTextFieldID() {
-		return textFieldID;
-	}
-
 }
